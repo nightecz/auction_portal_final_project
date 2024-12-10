f56635308d41b92d159b4b3e3aec843f20f79795
MartinPrudic <mprudic@gmail.com>
2024-12-10 10:01:04 +0100

    Function for auction creation - Form AuctionCreateForm - Template Auction_create - View AuctionCreateView

--- viewer/forms.py
+++ viewer/forms.py
@@ -3,12 +3,15 @@ import re
 from django.core.exceptions import ValidationError
 from django.forms import (
     CharField, DateField, Form, IntegerField, ModelChoiceField, Textarea, TextInput, EmailInput, PasswordInput,
-    ModelForm, DateInput, NumberInput
+    ModelForm, DateInput, NumberInput, CheckboxSelectMultiple
 )
 
 # from viewer.models import Auction
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+from viewer.models import Auction, Category
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
@@ -19,3 +22,22 @@ class SignUpForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field_name in self.fields:
             self.fields[field_name].widget.attrs['class'] = 'form-control'
+
+
+class AuctionCreateForm(ModelForm):
+    class Meta:
+        model = Auction
+        fields = ['name', 'description', 'starting_price', 'end_time', 'categories']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
+
+    def save(self, commit=True):
+        auction = super().save(commit=False)  # Nejprve uložíme aukci bez okamžitého commitu do DB
+        if not auction.current_price:
+            auction.current_price = auction.starting_price  # Pokud není current_price, nastavíme ho na starting_price
+        if commit:
+            auction.save()  # Uložíme aukci do databáze
+        return auction
