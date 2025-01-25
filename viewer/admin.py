faea8a1d1ef1b290e49f3a5755714acab19e706a
J <nighte@seznam.cz>
2025-01-25 19:36:11 +0100

    - cancel button of auction in the center now - cards are now all same (editet css), and have different colour of price - edited titles of auctions in index.html - base.html nav change colour - footer changed - now smaller and more minimalistic - in my_auctions.html boxes now have their own class - card-box-my-auction, buttons hover is black

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Auction, Profile, Watchlist, Bid, Purchase, Review
+from .models import Category, Auction, Profile, Watchlist, Bid, Purchase, Review, Archive
 
 admin.site.register(Auction)
 admin.site.register(Category)
@@ -8,3 +8,4 @@ admin.site.register(Watchlist)
 admin.site.register(Bid)
 admin.site.register(Purchase)
 admin.site.register(Review)
+admin.site.register(Archive)
